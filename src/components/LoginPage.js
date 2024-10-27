40254450110b0547a41189289354f0fede41820e
Mounika <mounikadn@gmail.com>
2024-10-27 11:01:22 +0530

    Login Validation changes

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,61 +1,105 @@
 import React, { useState } from "react";
-import { Button, Grid2, Input, Link, Paper, TextField } from "@mui/material";
-
-const data = [{
-  userName:"Mounika DN",
-  email:"mounika@gmail.com",
-  Password:"1234mouni"
-}]
+import {
+  Alert,
+  Button,
+  Grid2,
+  Input,
+  Link,
+  Paper,
+  TextField,
+} from "@mui/material";
+import './LoginPage.css';
+const Logindata = [
+  {
+    userName: "Mounika DN",
+    //email:"mounika@gmail.com",
+    Password: "1234mouni",
+  },
+];
 const LoginPage = () => {
   const paperStyle = {
     padding: 20,
     height: "100vh",
     width: 400,
-    margin: "20px auto",
+    margin: "0 auto",
   };
 
-  const [data,setData] = useState({userName:'',password:''})
+  const [data, setData] = useState({ userName: "", password: "" });
+  const [loginSubmit, setLoginSubmit] = useState(false);
 
-  const {userName,password} = data;
+  const { userName, password } = data;
   const handleChange = (e) => {
-    console.log(e,"Event")
-    console.log(e.target.name,e.target.value,"Target Values")
-    setData({...data,[e.target.name]:[e.target.value]})
-  }
+    if (e.target.name === "userName" && e.target.value.length > 25) {
+      setData({ ...data, [e.target.name]: [] });
+      console.log("Value entered is greater than 25 charecters");
+    } else {
+      setData({ ...data, [e.target.name]: [e.target.value] });
+    }
+  };
   const handleLoginsubmit = (e) => {
     e.preventDefault();
-    console.log(data,"DATA")
+    console.log(data,"DATA");
+    
+    setLoginSubmit(true);
+  };
+
+  const handleLogOutsubmit = (e) => {
+    e.preventDefault();
+   
+    setLoginSubmit(false);
+    
   };
   return (
-    <Paper elevation={5} style={paperStyle}>
-      <Grid2 container>
-        <label>Email or Mobile or Login Name</label>
-        <TextField
-          value={userName}
-          name='userName'
-          type="text"
-          fullWidth
-          onChange={handleChange}
-        />
-        <label>Password</label>
-        <TextField
-          value={password}
-          name="password"
-          onChange={handleChange}
-          fullWidth
-        />
-        <Link href="#" style={{ float: "right", padding: "10px 0px 10px 0px" }}>
-          Forget Login Credentials
-        </Link>
-        <Button
-          type="submit "
-          variant="contained"
-          color="primary"
-          onClick={handleLoginsubmit}
-          fullWidth
-        >
-          Login
-        </Button>
+    <Paper elevation={5} className="paperSection">
+      <Grid2 container className="login-Success-section">
+        {!loginSubmit ? (
+          <Grid2>
+            <label>Email or Mobile or Login Name</label>
+            <TextField
+              value={userName}
+              name="userName"
+              type="text"
+              fullWidth
+              onChange={handleChange}
+              maxLength={10}
+            />
+            <label>Password</label>
+            <TextField
+              value={password}
+              name="password"
+              onChange={handleChange}
+              fullWidth
+            />
+            <Link
+              href="#"
+              style={{ float: "right", padding: "10px 0px 10px 0px" }}
+            >
+              Forget Login Credentials
+            </Link>
+            <Button
+              type="submit "
+              variant="contained"
+              color="primary"
+              onClick={handleLoginsubmit}
+              fullWidth
+            >
+              Login
+            </Button>
+          </Grid2>
+        ) : (
+          <Grid2 className="logout-Success-section">
+            <label >Login Successful!!</label>
+            <Button
+              type="submit "
+              variant="contained"
+              color="primary"
+              onClick={handleLogOutsubmit}
+              fullWidth
+            >
+              LogOut
+            </Button>
+          </Grid2>
+        )}
       </Grid2>
     </Paper>
   );
